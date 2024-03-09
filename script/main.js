a1f6ada4874e5fa26fe0e7a2b43719ad678115bc
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-09 17:28:25 +0200

    Added some functionality for the API fetching

--- script/main.js
+++ script/main.js
@@ -1,1 +1,48 @@
-//Add Your Program here
+const recipeImage = document.querySelector("#recipeImage");
+// const video = document.querySelector("#video");
+const mealName = document.querySelector("#mealName");
+const mealCategory = document.querySelector("#mealCategory");
+const mealDescription = document.querySelector("#mealDescription");
+const ingredientOne = document.querySelector("#ingredientOne");
+const ingredientTwo = document.querySelector("#ingredientTwo");
+const ingredientThree = document.querySelector("#ingredientThree");
+const ingredientFour = document.querySelector("#ingredientFour");
+const ingredientFive = document.querySelector("#ingredientFive");
+const ingredientSix = document.querySelector("#ingredientSix");
+const ingredientEight = document.querySelector("#ingredientEight");
+const ingredientNine = document.querySelector("#ingredientNine");
+const ingredientTen = document.querySelector("#ingredientTen");
+
+const handleFetch = (searchValue) => {
+  const APIKEY_AND_APIURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
+  axios
+    .get(APIKEY_AND_APIURL)
+    .then((respond) => {
+      console.log(respond.data.meals[0]);
+      recipeImage.src = `${respond.data.meals[0].strMealThumb}`;
+      video.src = `${respond.data.meals[0].strYouTube}`;
+      mealName.innerHTML = respond.data.meals[0].strMeal;
+      mealCategory.innerHTML = respond.data.meals[0].strCategory;
+      mealDescription.innerHTML = respond.data.meals[0].strInstructions;
+      ingredientOne.innerHTML = `${respond.data.meals[0].strIngredient1}`;
+      ingredientTwo.innerHTML = `${respond.data.meals[0].strIngredient2}`;
+      ingredientThree.innerHTML = `${respond.data.meals[0].strIngredient3}`;
+      ingredientFour.innerHTML = `${respond.data.meals[0].strIngredient4}`;
+      ingredientFive.innerHTML = `${respond.data.meals[0].strIngredient5}`;
+      ingredientSix.innerHTML = `${respond.data.meals[0].strIngredient6}`;
+      ingredientSeven.innerHTML = `${respond.data.meals[0].strIngredient7}`;
+      ingredientEight.innerHTML = `${respond.data.meals[0].strIngredient8}`;
+      ingredientNine.innerHTML = `${respond.data.meals[0].strIngredient7}`;
+      ingredientTen.innerHTML = `${respond.data.meals[0].strIngredient10}`;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", () => {
+  const userInput = document.querySelector("#userInput").value;
+  handleFetch(userInput);
+});
